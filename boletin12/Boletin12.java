1a8126dbee462521293f8f743d4d24d7229cbb78
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-28 10:21:13 +0100

    Boletin12

---
+++ boletin12/Boletin12.java
@@ -0,0 +1,15 @@
+
+package boletin12;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin12 {
+
+    public static void main(String[] args) {
+        Garaje obx=new Garaje();
+        obx.programa();
+    }
+    
+}
